c61fee7afffaba9bbbe92c42d650c56c9e060b30
Eduardo Denis <edenis@frd.utn.edu.ar>
2023-10-03 19:57:50 -0300

    XMLHttpRequest

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hola Mundo!')
+let peticion = new XMLHttpRequest()
+peticion.open('GET', 'http://127.0.0.1:5500/header.html', true)
+peticion.onreadystatechange = () => {
+    if (peticion.readyState == 4 && peticion.status == 200) {
+        respuesta = peticion.response
+        document.body.insertAdjacentHTML("afterbegin", respuesta)
+    }
+}
+peticion.send()
